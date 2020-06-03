4fa99971aa48fdaac57706b99f39fbce8940481b
arvinq <arvin.quiliza@gmail.com>
2020-06-03 17:19:37 +1000

    Add ui properties to cell to output data

---
+++ NPWeather/NPWeather/Extension/String+ext.swift
@@ -0,0 +1,15 @@
+//
+//  String+ext.swift
+//  NPWeather
+//
+//  Created by Arvin Quiliza on 6/3/20.
+//  Copyright © 2020 arvinq. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var degree: String {
+        return "\(self)\u{00B0}"
+    }
+}
--- NPWeather/NPWeather/View/Cell/SuburbCollectionViewCell.swift
+++ NPWeather/NPWeather/View/Cell/SuburbCollectionViewCell.swift
@@ -12,6 +12,18 @@ class SuburbCollectionViewCell: UICollectionViewCell {
     
     static let reuseId = "SuburbCollectionViewCell"
     
+    var suburbNameLabel: UILabel!
+    var suburbConditionLabel: UILabel!
+    var suburbTemperatureLabel: UILabel!
+    
+    var textStackView: UIStackView!
+    
+    var suburbViewModel: SuburbViewModel? {
+        didSet {
+            bindValues()
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -28,11 +40,43 @@ class SuburbCollectionViewCell: UICollectionViewCell {
     }
     
     private func setupView() {
-        contentView.backgroundColor = .systemPink
+        
+        textStackView = UIStackView()
+        textStackView.axis = .vertical
+        textStackView.alignment = .fill
+        textStackView.distribution = .fillProportionally
+        textStackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(textStackView)
+        
+        suburbNameLabel = UILabel()
+        suburbNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        textStackView.addArrangedSubview(suburbNameLabel)
+        
+        suburbConditionLabel = UILabel()
+        suburbConditionLabel.translatesAutoresizingMaskIntoConstraints = false
+        textStackView.addArrangedSubview(suburbConditionLabel)
+        
+        suburbTemperatureLabel = UILabel()
+        suburbTemperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(suburbTemperatureLabel)
     }
     
     private func setupConstraints() {
-        
+        NSLayoutConstraint.activate([
+            textStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            textStackView.topAnchor.constraint(equalTo: topAnchor),
+            textStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            suburbTemperatureLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            suburbTemperatureLabel.topAnchor.constraint(equalTo: topAnchor),
+            suburbTemperatureLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    private func bindValues() {
+        suburbNameLabel.text = suburbViewModel?.suburbName
+        suburbConditionLabel.text = suburbViewModel?.weatherCondition ?? "Clear"
+        suburbTemperatureLabel.text = String(suburbViewModel?.weatherTemp ?? 0).degree
     }
     
 }
