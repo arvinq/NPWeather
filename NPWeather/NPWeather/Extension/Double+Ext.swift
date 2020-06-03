3b389de2be809841bb469995b03c031f0556cf37
arvinq <arvin.quiliza@gmail.com>
2020-06-03 15:58:10 +1000

    Added extension for alert and double date to string date conversion

---
+++ NPWeather/NPWeather/Extension/Double+Ext.swift
@@ -0,0 +1,24 @@
+//
+//  Double+Ext.swift
+//  NPWeather
+//
+//  Created by Arvin Quiliza on 6/3/20.
+//  Copyright © 2020 arvinq. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    var toStringDate: String {
+        get {
+            let date = Date(timeIntervalSince1970: self)
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.timeZone = TimeZone(abbreviation: "AEST")
+            dateFormatter.locale = NSLocale.current
+            dateFormatter.dateFormat = "h:mm a, dd MMMM yyyy"
+            
+            return dateFormatter.string(from: date)
+        }
+    }
+}
---
+++ NPWeather/NPWeather/Extension/UIViewController+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Ext.swift
+//  NPWeather
+//
+//  Created by Arvin Quiliza on 6/3/20.
+//  Copyright © 2020 arvinq. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func presentAlert(withTitle title: String, andMessage message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: buttonTitle, style: .cancel)
+            
+            alertController.addAction(alertAction)
+            self.present(alertController, animated: true)
+        }
+    }
+}
